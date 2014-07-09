f59280156def08b03dc0d2ae7c8f3d343afdc2f7
Nicolas <nikaz@hotmail.fr>
2014-07-09 16:01:17 +0200

    Format + Change comments in ECML Activity

--- ECML/src/com/ecml/ECMLActivity.java
+++ ECML/src/com/ecml/ECMLActivity.java
@@ -28,121 +28,110 @@ import android.graphics.drawable.ColorDrawable;
 
 import com.ecml.R;
 
-/** @class MidiSheetMusicActivity
- * This is the launch activity for MidiSheetMusic.
- * It simply displays the splash screen, and a button to choose a song.
+/**
+ * @class ECMLActivity
+ *        This is the launch activity for ECML.
+ *        It simply displays the splash screen with buttons leading to the
+ *        differents activities.
  */
 public class ECMLActivity extends Activity {
-	
+
 	final Context context = this;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setTheme(android.R.style.Theme_Holo_Light);
-        setContentView(R.layout.main);
-        
-        //Set Actionbar color
-        ActionBar ab = getActionBar();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTheme(android.R.style.Theme_Holo_Light);
+		setContentView(R.layout.main);
+
+		// Set Actionbar color
+		ActionBar ab = getActionBar();
 		ColorDrawable colorDrawable = new ColorDrawable(getResources().getColor(R.color.orange));
 		ab.setBackgroundDrawable(colorDrawable);
-       
-        //Choose song button
-        ImageView chooseSong = (ImageView) findViewById(R.id.choose_song);
-        chooseSong.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View v) {
-                    	Intent intent = new Intent(getApplicationContext(), ChooseSongActivity.class);
-                        startActivity(intent);
-                    }
-                }
-        		);
-                
-        //Audio Recording button
-                ImageView audioRecording = (ImageView) findViewById(R.id.audiorecording);
-                audioRecording.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToAudio = new Intent(getApplicationContext(), AudioRecordingActivity.class);
-                        		startActivity(goToAudio);
-                            }
-                        } );       
-        
-        //Video Recording button
-                ImageView videoRecording = (ImageView) findViewById(R.id.videorecording);
-                videoRecording.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToVideo = new Intent(getApplicationContext(), VideoRecordingActivity.class);
-                        		startActivity(goToVideo);
-                            }
-                        } );       
-                
-        //Calendar button
-                ImageView calendar = (ImageView) findViewById(R.id.calendar);
-                calendar.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToCalendar = new Intent(getApplicationContext(), CalendarActivity.class);
-                        		startActivity(goToCalendar);
-                            }
-                        } );
-                
-       //Game button
-                ImageView game = (ImageView) findViewById(R.id.game);
-                game.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToGame = new Intent(getApplicationContext(), GameActivity.class);
-                        		startActivity(goToGame);
-                            }
-                        } );
-                
-       //Metronome button
-                ImageView metronome = (ImageView) findViewById(R.id.metronome);
-                metronome.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToMetronome = new Intent(getApplicationContext(), MetronomeActivity.class);
-                        		startActivity(goToMetronome);
-                            }
-                        } );
-                
-        //Tuning fork button
-                ImageView tuning = (ImageView) findViewById(R.id.tuning);
-                tuning.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Toast.makeText(context, "Tuning fork", Toast.LENGTH_SHORT).show();
-                        		MediaPlayer mPlayer = MediaPlayer.create(context, R.raw.tuning);
-                        		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                        		try {
-                        			mPlayer.prepare();
-                        		} catch (IllegalStateException e) {
-                        			// TODO Auto-generated catch block
-                        			e.printStackTrace();
-                        		} catch (IOException e) {
-                        			// TODO Auto-generated catch block
-                        			e.printStackTrace();
-                        		}
-                        		mPlayer.start();
-                            }
-                        } );
-                
-         //Youtube button
-                ImageView youtube = (ImageView) findViewById(R.id.youtube);
-                youtube.setOnClickListener(
-                        new View.OnClickListener() {
-                            public void onClick(View v) {
-                            	Intent goToYoutube = new Intent(getApplicationContext(), YoutubeActivity.class);
-                        		startActivity(goToYoutube);
-                            }
-                        } );
-                
-          
 
-    }
+		// Choose song button
+		ImageView chooseSong = (ImageView) findViewById(R.id.choose_song);
+		chooseSong.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), ChooseSongActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		// Audio Recording button
+		ImageView audioRecording = (ImageView) findViewById(R.id.audiorecording);
+		audioRecording.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToAudio = new Intent(getApplicationContext(), AudioRecordingActivity.class);
+				startActivity(goToAudio);
+			}
+		});
+
+		// Video Recording button
+		ImageView videoRecording = (ImageView) findViewById(R.id.videorecording);
+		videoRecording.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToVideo = new Intent(getApplicationContext(), VideoRecordingActivity.class);
+				startActivity(goToVideo);
+			}
+		});
+
+		// Calendar button
+		ImageView calendar = (ImageView) findViewById(R.id.calendar);
+		calendar.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToCalendar = new Intent(getApplicationContext(), CalendarActivity.class);
+				startActivity(goToCalendar);
+			}
+		});
+
+		// Game button
+		ImageView game = (ImageView) findViewById(R.id.game);
+		game.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToGame = new Intent(getApplicationContext(), GameActivity.class);
+				startActivity(goToGame);
+			}
+		});
+
+		// Metronome button
+		ImageView metronome = (ImageView) findViewById(R.id.metronome);
+		metronome.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToMetronome = new Intent(getApplicationContext(), MetronomeActivity.class);
+				startActivity(goToMetronome);
+			}
+		});
+
+		// Tuning fork button
+		ImageView tuning = (ImageView) findViewById(R.id.tuning);
+		tuning.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(context, "Tuning fork", Toast.LENGTH_SHORT).show();
+				MediaPlayer mPlayer = MediaPlayer.create(context, R.raw.tuning);
+				mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				try {
+					mPlayer.prepare();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				mPlayer.start();
+			}
+		});
+
+		// Youtube button
+		ImageView youtube = (ImageView) findViewById(R.id.youtube);
+		youtube.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent goToYoutube = new Intent(getApplicationContext(), YoutubeActivity.class);
+				startActivity(goToYoutube);
+			}
+		});
+	}
 
 
 }
-
--- ECML/src/com/metronome/MetronomeController.java
+++ ECML/src/com/metronome/MetronomeController.java
@@ -5,7 +5,7 @@ import com.ecml.MetronomeActivity;
 import com.ecml.SheetMusicActivity;
 
 public class MetronomeController {
-	
+
 	private SheetMusicActivity metronomeSheetMusicActivity;
 	private MetronomeActivity metronomeActivity;
 	private Metronome metronome;
@@ -15,39 +15,39 @@ public class MetronomeController {
 		this.metronomeSheetMusicActivity = metronomeSheetMusicActivity;
 		metronome = new Metronome();
 	}
-	
+
 	public MetronomeController(MetronomeActivity metronomeActivity) {
 		this.metronomeActivity = metronomeActivity;
 		metronome = new Metronome();
 	}
-	
-	public int getTempo(){
+
+	public int getTempo() {
 		return metronome.getTempo();
 	}
-	
+
 	public void setTempo(int tempo) {
-		metronome.setTempo(tempo+1);
+		metronome.setTempo(tempo + 1);
 	}
-	
-	public int getAccentBeep(){
+
+	public int getAccentBeep() {
 		return metronome.getAccentBeep();
 	}
-	
+
 	public void setAccentBeep(int AccentBeep) {
 		metronome.setAccentBeep(AccentBeep);
 	}
-	
+
 	public void startMetronome() {
-		if(horloge != null) {
+		if (horloge != null) {
 			horloge.stop();
 		}
 		horloge = new Horloge(metronome.getTempo(), metronome.getAccentBeep(), metronomeActivity.getBaseContext());
 	}
-	
+
 	public void stopMetronome() {
-		if(horloge != null) {
+		if (horloge != null) {
 			horloge.stop();
 		}
 	}
-	
+
 }
